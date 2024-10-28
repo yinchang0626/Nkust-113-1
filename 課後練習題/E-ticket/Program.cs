1a15b06a5a48ddd692aedd851877e9e7b3a391b4
DESKTOP-EQHNCHB\USER <F112152134@nkust.edu.tw>
2024-10-28 19:09:04 +0800

    Generate new project name "E-ticket", download open data "高雄都會區大眾捷運系統電子票證發行量及交易量.csv" and create file "Program.cs"

---
+++ 課後練習題/E-ticket/Program.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using System.Globalization;
+using Analysis;
+
+namespace E_ticket
+{
+    class Program
+    {
+        static void Main()
+        {
+            using var reader = new StreamReader("高雄都會區大眾捷運系統電子票證發行量及交易量.csv");
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            
+            var records = csv.GetRecords<dynamic>();
+            foreach (var record in records)
+            {
+                var dict = (IDictionary<string, object>)record;
+                int sum = Convert.ToInt32(dict["交易筆數-撘乘渡輪"]) + Convert.ToInt32(dict["交易筆數-搭乘公車"]) + Convert.ToInt32(dict["交易筆數-搭乘捷運"]);
+                int data = Convert.ToInt32(dict["交易筆數-合計"]);
+                if(sum == data)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine($"False, sum = {sum}, data = {data}");
+                }
+            }
+        }
+    }
+}
