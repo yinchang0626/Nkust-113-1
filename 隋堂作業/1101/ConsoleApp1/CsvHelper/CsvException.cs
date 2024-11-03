3e4f625226d391c472ef4d58d715f9b7dc2f5728
Gary1804g <C110152333@nkust.edu.tw>
2024-11-04 00:15:10 +0800

    Add files via upload

---
+++ 隋堂作業/1101/ConsoleApp1/CsvHelper/CsvException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CsvHelper
+{
+    [Serializable]
+    internal class CsvException : Exception
+    {
+        public CsvException()
+        {
+        }
+
+        public CsvException(string message) : base(message)
+        {
+        }
+
+        public CsvException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CsvException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
