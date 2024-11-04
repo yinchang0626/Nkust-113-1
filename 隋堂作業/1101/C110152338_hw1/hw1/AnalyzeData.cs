0dde9b550803facfbf4a7c3095938353bd0e3b7b
kerong <krameri120@gmail.com>
2024-11-04 09:04:18 +0800

    hw1_upload

---
+++ 隋堂作業/1101/C110152338_hw1/hw1/AnalyzeData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace hw1
+{
+    public class AnalyzeData
+    {
+        public int totalRecords { get; set; }
+        public int totalMale { get; set; }
+        public int totalFemale { get; set; }
+        public int maxMale { get; set; }
+        public int minMale { get; set; }
+        public int maxFemale { get; set; }
+        public int minFemale { get; set; }
+        public double avgMale { get; set; }
+        public double avgFemale { get; set; }
+
+        // 建構函數
+        public AnalyzeData()
+        {
+            // 預設值
+            totalRecords = 0;
+            totalMale = 0;
+            totalFemale = 0;
+            maxMale = int.MinValue;
+            minMale = int.MaxValue;
+            maxFemale = int.MinValue;
+            minFemale = int.MaxValue;
+            avgMale = 0.0;
+            avgFemale = 0.0;
+        }
+        ~AnalyzeData()
+        {
+
+        }
+    }
+}
