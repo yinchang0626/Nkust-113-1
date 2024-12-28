b67847dfebf9ec4dfdd10ee7908c89fa05ff0c0b
kerong <krameri120@gmail.com>
2024-12-28 10:53:54 +0800

    view and controller

---
+++ 期末作業/WebApplication1/WebApplication1/Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 期末作業/WebApplication1/WebApplication1/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public string Id {  get; set; }
+        public Address? AddressP { get; set; }  //可能為null
+        public int? Pace { get; set; } //步
+        public int? Mileage { get; set;} //里程
+
+        public ICollection<Club> clubs { get; set; } //集合屬性
+
+        public ICollection<Races> races { get; set; } //集合屬性
+    }
+}
