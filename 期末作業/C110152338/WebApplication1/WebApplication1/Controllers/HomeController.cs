908f1b840a3e95819da6e95b741f5f08041c8a8b
kerong <krameri120@gmail.com>
2024-12-28 20:08:15 +0800

    Personal Profile view update

--- 期末作業/C110152338/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ 期末作業/C110152338/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
