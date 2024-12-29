3adc47e411030bd58c2be64745233d59ed33106f
odinx123 <odinhsiu68@gmail.com>
2024-12-29 11:53:48 +0800

    初始化 Asp.net core 專案

---
+++ 期末作業/C110152318_期末作業/final_project/final_project/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace final_project.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Street { get; set; }
+        public String City { get; set; }
+        public String state { get; set; }
+    }
+}
---
+++ 期末作業/C110152318_期末作業/final_project/final_project/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace final_project.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+    }
+}
