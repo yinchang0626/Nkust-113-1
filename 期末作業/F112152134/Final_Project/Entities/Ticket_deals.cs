7f40091e64996c8690120681e67fbb47335cf740
DESKTOP-EQHNCHB\USER <F112152134@nkust.edu.tw>
2024-12-30 22:10:14 +0800

    Define all fields of opendata in "Ticket_deals.cs"

--- 期末作業/F112152134/Final_Project/Entities/Ticket_deals.cs
+++ 期末作業/F112152134/Final_Project/Entities/Ticket_deals.cs
@@ -1,6 +1,15 @@
-﻿namespace Final_Project.Entities
+﻿using Microsoft.VisualBasic;
+
+namespace Final_Project.Entities
 {
     public class Ticket_deals
     {
+        public string Year { get; set; } = null!;
+        public int Cumulative_amount { get; set; }
+        public int Deal_payment { get; set; }
+        public int Quantity_total { get; set; }
+        public int Quantity_metro { get; set; }
+        public int Quantity_bus { get; set; }
+        public int Quantity_boat { get; set; }
     }
 }
