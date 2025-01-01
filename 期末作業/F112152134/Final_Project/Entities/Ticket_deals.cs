1c050bc8e2f917ed8e2b4af7a5ab390f7c92dc78
DESKTOP-EQHNCHB\USER <F112152134@nkust.edu.tw>
2025-01-01 22:04:23 +0800

    Read opendata into database

--- 期末作業/F112152134/Final_Project/Entities/Ticket_deals.cs
+++ 期末作業/F112152134/Final_Project/Entities/Ticket_deals.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.VisualBasic;
+﻿using System.ComponentModel.DataAnnotations;
+using CsvHelper.Configuration.Attributes;
+using Microsoft.VisualBasic;
 
 namespace Final_Project.Entities
 {
     public class Ticket_deals
     {
-        public int Id { get; set; }
-        public string Year { get; set; } = null!;
-        public int Cumulative_amount { get; set; }
-        public int Deal_payment { get; set; }
-        public int Quantity_total { get; set; }
-        public int Quantity_metro { get; set; }
-        public int Quantity_bus { get; set; }
-        public int Quantity_boat { get; set; }
+        [Key]
+        [Name("年分")] public string Year { get; set; } = null!;
+        [Name("累計發卡量")] public int Cumulative_amount { get; set; }
+        [Name("交易金額")] public int Deal_payment { get; set; }
+        [Name("交易筆數-合計")] public int Quantity_total { get; set; }
+        [Name("交易筆數-搭乘捷運")] public int Quantity_metro { get; set; }
+        [Name("交易筆數-搭乘公車")] public int Quantity_bus { get; set; }
+        [Name("交易筆數-撘乘渡輪")] public int Quantity_boat { get; set; }
     }
 }
