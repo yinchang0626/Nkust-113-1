81f48e2b330ece3a5fc39abe0c8dd310b1013a47
unknownwyj <wongyijin11@gmail.com>
2025-01-03 17:28:39 +0800

    mvp without login

---
+++ 期末作業/C111152353/mvc/mvc/Controllers/Crime_dataController.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mvc.Data;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class Crime_dataController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Crime_dataController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Crime_data
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Crime_data.ToListAsync());
+        }
+
+        // GET: Crime_data/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var crime_data = await _context.Crime_data
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (crime_data == null)
+            {
+                return NotFound();
+            }
+
+            return View(crime_data);
+        }
+
+        // GET: Crime_data/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Crime_data/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Type,Year,Date,Country,Region")] Crime_data crime_data)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(crime_data);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(crime_data);
+        }
+
+        // GET: Crime_data/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var crime_data = await _context.Crime_data.FindAsync(id);
+            if (crime_data == null)
+            {
+                return NotFound();
+            }
+            return View(crime_data);
+        }
+
+        // POST: Crime_data/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Type,Year,Date,Country,Region")] Crime_data crime_data)
+        {
+            if (id != crime_data.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(crime_data);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!Crime_dataExists(crime_data.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(crime_data);
+        }
+
+        // GET: Crime_data/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var crime_data = await _context.Crime_data
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (crime_data == null)
+            {
+                return NotFound();
+            }
+
+            return View(crime_data);
+        }
+
+        // POST: Crime_data/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var crime_data = await _context.Crime_data.FindAsync(id);
+            if (crime_data != null)
+            {
+                _context.Crime_data.Remove(crime_data);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadCsv(IFormFile csvFile)
+        {
+            if (csvFile != null && csvFile.Length > 0)
+            {
+                using (var stream = new StreamReader(csvFile.OpenReadStream()))
+                {
+                    // Process the CSV file and save data to the database
+                    // Example: Read the CSV file line by line
+                    while (!stream.EndOfStream)
+                    {
+                        var line = await stream.ReadLineAsync();
+                        var values = line.Split(',');
+                        // remove the first line
+                        if (values[0] == "案類" || values[0] == "type")
+                        {
+                            continue;
+                        }
+                        if (values.Length != 5)
+                        {
+                            // Invalid data
+                            continue;
+                        }
+                        // Save values to the database
+                        // data required to be in the format of "yyyy-MM-dd"
+                        var monthint = int.Parse(values[2].Substring(0, 2));
+                        var dayint = int.Parse(values[2].Substring(2, 2));
+                        if (values[2].Length >= 4 && monthint <= 12 && dayint <= 31 && monthint > 0 && dayint > 0)
+                        {
+                            var nyear = int.Parse(values[1]) + 1911;
+                            var syear = nyear.ToString(); 
+                            var sdate = syear + "-" + values[2].Substring(0, 2) + "-" + values[2].Substring(2, 2);
+                            var crime_data = new Crime_data
+                            {
+                                Type = values[0],
+                                Year = int.Parse(values[1]),
+                                Date = DateTime.Parse(sdate),
+                                Country = values[3],
+                                Region = values[4]
+                            };
+                            // Add code to save crime_data to the database
+                            _context.Add(crime_data);
+                            await _context.SaveChangesAsync();
+                        }
+                        else
+                        {
+                            // Handle invalid date format
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+        private bool Crime_dataExists(int id)
+        {
+            return _context.Crime_data.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ 期末作業/C111152353/mvc/mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using mvc.Models;
+
+namespace mvc.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<mvc.Models.Crime_data> Crime_data { get; set; } = default!;
+    }
+}
