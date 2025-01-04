2a17dcc7f82b87745c94a138468c39f6c9268568
lzx_ovo <lzx2003227@gmail.com>
2025-01-05 00:56:32 +0800

    建置網頁

---
+++ 期末作業/C111152301/軟工期末/project/project/Controllers/stock.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project.Controllers
+{
+    public class stock : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
