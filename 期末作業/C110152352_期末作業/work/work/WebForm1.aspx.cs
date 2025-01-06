10ef7f0c9ca69222547ee95e806f82cf631c550c
LAPTOP-6FNPONAS\user <C110152352@nkust.edu.tw>
2025-01-06 14:23:19 +0800

    C110152352

---
+++ 期末作業/C110152352_期末作業/work/work/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace work
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // 頁面首次加載時，不處理查詢
+            if (!IsPostBack)
+            {
+                SqlDataSource2.SelectParameters["供應商"].DefaultValue = string.Empty;
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            // 更新查詢 GridView 的數據
+            SqlDataSource2.SelectParameters["供應商"].DefaultValue = txtSearch.Text.Trim();
+            GridView2.DataBind(); // 重新綁定查詢結果的資料
+        }
+    }
+}
