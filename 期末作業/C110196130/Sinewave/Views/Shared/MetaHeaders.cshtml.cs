be818cccdf3892968bcb2e4c66a8fb0c030208f0
5j5fuzc <100426488+5j5fuzc@users.noreply.github.com>
2025-01-10 01:07:30 +0800

    Add files via upload

---
+++ 期末作業/C110196130/Sinewave/Views/Shared/MetaHeaders.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sinewave.Views.Shared;
+public class MetaHeaders : PageModel
+{
+    public MarkdownFileBase Doc { get; set; }
+    public MetaHeaders(MarkdownFileBase doc) => Doc = doc;
+
+    public string? CanonicalUrl { get; set; }
+    public AuthorInfo? Author { get; set; }
+    public string? Image { get; set; }
+}
