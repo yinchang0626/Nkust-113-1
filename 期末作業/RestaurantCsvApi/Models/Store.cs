24ca508455e9ad6f58a920866c0f7d8719a4ea04
Gary1804g <C110152333@nkust.edu.tw>
2025-01-09 23:59:11 +0800

    Add files via upload

---
+++ 期末作業/RestaurantCsvApi/Models/Store.cs
@@ -0,0 +1,10 @@
+public class Store
+{
+    public int 序號 { get; set; }
+    public string 區域 { get; set; }
+    public string 店家 { get; set; }
+    public string 報名組別 { get; set; }
+    public string 營業地址 { get; set; }
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
+}
