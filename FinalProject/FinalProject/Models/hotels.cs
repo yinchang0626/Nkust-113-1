6a9f7223128b968177200f7331b168fb504be8f1
jerrychen12315 <77978298+jerrychen12315@users.noreply.github.com>
2025-01-10 08:29:07 +0800

    Add files via upload

---
+++ FinalProject/FinalProject/Models/hotels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalProject.Models;
+
+public partial class hotels
+{
+    public byte id { get; set; }
+
+    public string _class { get; set; } = null!;
+
+    public string? star { get; set; }
+
+    public string name { get; set; } = null!;
+
+    public string? district { get; set; }
+
+    public string address { get; set; } = null!;
+
+    public string tel { get; set; } = null!;
+
+    public short? amount { get; set; }
+
+    public string? email { get; set; }
+
+    public string? website { get; set; }
+
+    public double lng { get; set; }
+
+    public double lat { get; set; }
+}
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-113-1-軟體工程
+# 期末作業
+
+## C110152237
+
+https://youtu.be/nsI-8mxEUvQ
+
+一開始只能看到部分資訊
+![image](image\初始.png)
+
+登入會員後可以看到詳細資訊
+![image](image\會員.png)
+
+這邊因為驗證的功能來不及做，所以透過直接改網址的部分來切換帳號
+![image](image\詳細資訊.png)
+
+使用管理員帳號可以有更多權限
+![image](image\管理員.png)
+
+新增資料
+![image](image\創建.png)
+![image](image\創建成功.png)
+
+更改資料
+![image](image\編輯.png)
+![image](image\編輯成功.png)
+
+刪除資料
+![image](image\刪除.png)
+![image](image\刪除成功.png)
