358f29974d9357eee579435b587e2fd910ae4175
Zhang <C110152307@nkust.edu.tw>
2025-01-10 10:05:34 +0800

    add hw

---
+++ 期末作業/C110152307/final_project/WebApplication4/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(string id, string password)
+        {
+            ViewBag.id = id;
+            ViewBag.password = password;
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Member member)
+        {
+            return View(member);
+        }
+    }
+}
---
+++ 期末作業/C110152307/final_project/WebApplication4/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Member
+    {
+        public string Id { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+    }
+}
