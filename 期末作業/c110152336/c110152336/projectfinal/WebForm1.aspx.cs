063933d6cb443fbc564338b450e486e67ba1408d
LAPTOP-5LHVTTK6\h9205 <C110152336>
2025-01-11 14:50:52 +0800

    C110152336 期末作業

---
+++ 期末作業/c110152336/c110152336/projectfinal/WebForm1.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Xml.Linq;
+
+namespace projectfinal
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["taiwanConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM [speed$]"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string customerId = Convert.ToString(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string constr = ConfigurationManager.ConnectionStrings["taiwanConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM [speed$] WHERE 單位名稱 = @CustomerId"))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerId", customerId);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindGrid();
+        }
+
+        protected void Insert(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string country = txtPost.Text;
+            string addr = txtAddr.Text;
+            string phone = txtPhone.Text;
+            txtName.Text = "";
+            txtPost.Text = "";
+            txtAddr.Text="";
+            txtPhone.Text="";
+            string query = "INSERT INTO [speed$] VALUES(@Name, @Post,@Addr,@Phone)";
+            string constr = ConfigurationManager.ConnectionStrings["taiwanConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Post", country);
+                    cmd.Parameters.AddWithValue("@Addr", addr);
+                    cmd.Parameters.AddWithValue("@Phone", phone);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindGrid();
+        }
+    }
+}
